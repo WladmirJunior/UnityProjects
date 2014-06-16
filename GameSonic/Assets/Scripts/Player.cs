f50350f568e3ed6aee71744a4c70dc85b7954775
WladmirJunior <wlad.w.junior@gmail.com>
2014-06-16 07:26:45 -0300

    Input Touch in Unity

---
+++ GameSonic/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+
+    public bool jumping = false;
+
+    public int itens = 0;
+    
+    private float motion;
+    public float Motion
+    {
+        get { return motion; }
+        set { motion = Mathf.Clamp(value, -1, 1); }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(motion * speed, rigidbody2D.velocity.y);
+        Debug.Log("Motion: "+ motion);
+    }
+
+    void Update()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+        // Verifica se esta pulando ou caindo pela sua forca vertical
+        jumping = rigidbody2D.velocity.y != 0;
+    }
+
+    public void Jump()
+    {
+        if (!jumping)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }       
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(100,100,200,100), "Itens: "+itens);
+    }
+
+}
---
+++ TouchInput/Assets/Scritps/TouchInput.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum Motion
+{
+    FORWARD,
+    BACK,
+    IDLE,
+}
+
+public class TouchInput : MonoBehaviour {
+
+    public GUITexture btnBack, btnForward, btnJump;
+
+    private Texture backNormal, ForwardNormal, jumpNormal;
+    public Texture backHold, ForwardHold, jumpHold;
+
+    public Player controllerPlayer;
+    public float deltaAxis;
+
+    public Motion motionState = Motion.IDLE;
+
+    private int touchJump;
+
+    void Awake()
+    {
+        backNormal = btnBack.texture;
+        ForwardNormal = btnForward.texture;
+        jumpNormal = btnJump.texture;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount <= 0)
+        {
+            if ((motionState == Motion.BACK || motionState == Motion.FORWARD) && (!controllerPlayer.jumping))
+            {
+                controllerPlayer.Motion = 0;
+                motionState = Motion.IDLE;
+            }
+
+            btnJump.texture = jumpNormal;
+            //Debug.Log("Não tem toque na tela!");
+        }
+        else
+        {
+            // Percorre todos os toques de tela
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+
+
+
+                // Máquina de estados para o botao back
+                if (btnBack.guiTexture.HitTest(Input.GetTouch(i).position))
+                {
+                    //btnBack.texture = backHold;
+                    controllerPlayer.Motion -= deltaAxis;
+                    motionState = Motion.BACK;
+                }
+                //else if (motionState == Motion.BACK && !controllerPlayer.jumping)
+                //{
+                //    //Debug.Log("Tirou o dedo de andar para tras");
+                //    controllerPlayer.Motion = 0;
+                //}
+
+                // Máquina de estados para o botao forward
+                else if (btnForward.guiTexture.HitTest(Input.GetTouch(i).position))
+                {
+                    controllerPlayer.Motion += deltaAxis;
+                    motionState = Motion.FORWARD;
+                }
+                //else if (motionState == Motion.FORWARD && !controllerPlayer.jumping)
+                //{
+                //    //Debug.Log("Tirou o dedo de andar para frente");
+                //    controllerPlayer.Motion = 0;
+                //}
+
+                //// Máquina de estados para o botao jump
+                else if (btnJump.guiTexture.HitTest(Input.GetTouch(i).position))
+                {
+                    //Debug.Log("Salto");
+                    // Jump
+                    touchJump = i;
+                    btnJump.texture = jumpHold;
+                    controllerPlayer.Jump();
+                    controllerPlayer.jumping = true;
+                }
+                else
+                {
+                    btnJump.texture = jumpNormal;
+                }
+            }
+
+            //if (!btnJump.guiTexture.HitTest(Input.GetTouch(touchJump).position))
+            //{
+            //    Debug.Log("Tirou o dedo do botao jump");
+            //}
+
+        }
+
+        if (controllerPlayer.Motion == 0) { motionState = Motion.IDLE; }
+
+        switch (motionState)
+        {
+            case Motion.IDLE:
+                btnBack.texture = backNormal;
+                btnForward.texture = ForwardNormal;
+                break;
+            case Motion.BACK:
+                btnForward.texture = ForwardNormal;
+                btnBack.texture = backHold;
+                break;
+            case Motion.FORWARD:
+                btnBack.texture = backNormal;
+                btnForward.texture = ForwardHold;
+                break;
+        }
+
+    }
+
+
+
+
+}
